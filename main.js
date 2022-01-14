48c86c578da8dc12a973b331b0b05288a456f7d6
hussein <husseingh732@gmail.com>
2022-01-15 01:09:13 +0200

    simple change on je file

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let section = document.querySelector(".skills")
 let pspan =  document.querySelectorAll(".skills .progress span[data-widths]")
 window.onscroll = function ()
 {
-    if (window.scrollY >= section.offsetTop + 50)
+    if (window.scrollY >= section.offsetTop + 20)
     {
         spans.forEach(element => {
             element.style.width = element.getAttribute("data-width")
